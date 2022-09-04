ec32f16a25a3080a0a46a4faa742dd54e1191835
munyaja <mury220@mhs.unsyiah.ac.id>
2022-09-04 18:24:12 +0700

    latihan pertama TKI

---
+++ simple_webcrawling/index.php
@@ -0,0 +1,13 @@
+<?php  
+function crawl_page($url,$depth=2){
+    if($depth>0){
+        $html = file_get_contents($url);
+        preg_match_all('~<a/*?href="(.*?)".?>~',$html, $matches);
+        foreach($matches[1] as $newurl){
+            crawl_page($newurl,$depth-1);   
+        }
+        file_put_contents('result.html',"\n\n".$html."\n\n",FILE_APPEND);
+    }
+}
+
+crawl_page('https://www.acehnews.id/category/politik',2);
